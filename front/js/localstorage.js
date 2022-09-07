854e2ac0d1c28ed5c4f5113d9da0d3d06b704a63
bicisse <105852207+bicisse@users.noreply.github.com>
2022-09-07 14:13:55 +0200

    local storage fini

---
+++ front/js/localstorage.js
@@ -0,0 +1,228 @@
+
+//______________________________
+
+
+document.getElementById('addToCart').addEventListener('click', function(){
+ //Couleur et quantité*************************
+    //quantity
+    let quantityChoice = document.getElementById('quantity').value;
+        // console.log(typeof quantityChoice); //string
+    const quantity = Number(quantityChoice);
+        // console.log(typeof quantity);   // number
+    
+
+    //color
+    const colorChoice = document.getElementById('color-select'); 
+    const color = colorChoice.options[colorChoice.selectedIndex].value; 
+
+    //choice
+    const choice = {color: color, quantity: quantity};
+    const stringifyChoice = JSON.stringify(choice);
+    
+    // Local storage
+    let currentLs;
+    let emptyArray =[];
+    function getFromLocalStorage(){
+        
+        currentLs = localStorage.getItem(extractId);
+        return currentLs;
+    }
+    
+    
+    // function makestring(makeString) {
+    //     const strinfgified = JSON.stringify(makeString);
+    //     console.log('strinfgified',strinfgified, typeof strinfgified);
+    //     return strinfgified;
+    // }
+
+    // function makeStringThenSetInLS(makeString)  {
+    //     // const strinfgified = JSON.stringify(makeString);
+    //     // console.log('strinfgified',strinfgified, typeof strinfgified);
+    //     emptyArray.push(makeString);
+    //     console.log("emptyArray", emptyArray, typeof emptyArray);
+    //     const arrayLs = JSON.stringify(emptyArray);
+    //     console.log("arrayLs", arrayLs, typeof arrayLs);
+    
+    //     const setInLocalStorage = localStorage.setItem(extractId, arrayLs);
+    //     return setInLocalStorage;
+        
+    // }
+
+    let firstValue;
+    function firstPushInEmptyArray(makeString){
+        
+        emptyArray.push(makeString);
+        console.log("emptyArray", emptyArray, typeof emptyArray);
+        firstValue = JSON.stringify(emptyArray);
+        return firstValue;
+    }
+    let setInLocalStorage;
+    function makeStringThenSetInLS(value)  {
+        console.log(value);
+        setInLocalStorage = localStorage.setItem(extractId, value);
+        return setInLocalStorage;
+    }
+ 
+// LOCAL STORAGE
+
+
+    // if a color is picked 
+    if (color !== ""){
+
+
+        // there is a key with this ID
+        if( localStorage.getItem(extractId)){
+            console.log("This Kanap has been selected before...");
+            console.log(choice, stringifyChoice);
+
+            getFromLocalStorage();
+            console.log('currentLs',currentLs);// get the array of the object with this page id
+            const parseCurrentLs = JSON.parse(currentLs);
+            // console.log(parseCurrentLs);
+
+            
+            //Parse the content of the local storage
+            
+
+
+            //
+            const colorIndex = parseCurrentLs.findIndex(object => {
+                return object.color === choice.color;
+            //
+
+            })
+            console.log('colorIndex', colorIndex);
+            
+            if (colorIndex === -1) {
+                //same color
+                console.log("... but with another color. Let's add this one to the local sto.");
+                parseCurrentLs.push(choice);
+                // console.log('parseCurrentLs', parseCurrentLs);
+                
+                const stringifyNewChoice = JSON.stringify(parseCurrentLs);
+                const backInLocalStorage= localStorage.setItem(extractId, stringifyNewChoice)
+                
+            } else {
+                //different colors
+                    const objectToUpdate = parseCurrentLs[colorIndex];
+                    console.log('objectToUpdate', objectToUpdate);
+                    currentQuantity = objectToUpdate.quantity;
+                    console.log(currentQuantity);
+                    console.log("...with this exact color. Let's update the quantity.");
+                    
+                    const updatedQuantity = currentQuantity + choice.quantity;
+                    // console.log(updatedQuantity);  
+                    
+                    const newChoice = {color : choice.color, quantity: updatedQuantity};
+                    
+                    // console.log('parseCurrentLs', parseCurrentLs);
+                    removedEl= parseCurrentLs.splice(colorIndex, 1);
+                    // console.log('parseCurrentLs', parseCurrentLs);
+                    // console.log( 'removedEl', removedEl);
+
+                    parseCurrentLs.push(newChoice);
+                    // console.log('parseCurrentLs', parseCurrentLs);
+
+                    const stringifyNewChoice = JSON.stringify(parseCurrentLs);
+                    const backInLocalStorage= localStorage.setItem(extractId, stringifyNewChoice)
+                        
+                    
+               }
+               // __________Fin du else
+                // const sameColor = parseCurrentLs[i].color === choice.color;
+                // console.log('sameColor',sameColor);
+                
+                // const lsColor= parseCurrentLs[i].color;
+                // // console.log(lsColor, 'lsColor');
+                // const currentQuantity= parseCurrentLs[i].quantity;
+                // // console.log(currentQuantity, 'currentQuantity');
+                // // console.log(choice.color, 'choice.color');
+                // // console.log(choice.quantity, 'choice.quantity');
+                
+                // //_______________
+
+
+           
+                // //___________________
+                // if(sameColor){
+                    
+                //     console.log('Same color.Update quantity');
+                //     const updatedQuantity = currentQuantity + choice.quantity;
+                //     // console.log(updatedQuantity);  
+                    
+                //     const newChoice = {color : choice.color, quantity: updatedQuantity};
+                    
+                //     // console.log('parseCurrentLs', parseCurrentLs);
+                //     removedEl= parseCurrentLs.splice(indexOfObject, 1);
+                //     // console.log('parseCurrentLs', parseCurrentLs);
+                //     // console.log( 'removedEl', removedEl);
+
+                //     parseCurrentLs.push(newChoice);
+                //     // console.log('parseCurrentLs', parseCurrentLs);
+
+                //     const stringifyNewChoice = JSON.stringify(parseCurrentLs);
+                //     const backInLocalStorage= localStorage.setItem(extractId, stringifyNewChoice)
+                    
+
+
+
+                // } else {
+                //     console.log('different colors');
+                //     console.log('indexOfObject2', indexOfObject2);
+                //     if( indexOfObject2){
+                //         console.log('different color already in the array. Update');
+                //         const updatedQuantity = currentQuantity + choice.quantity;
+                //     // console.log(updatedQuantity);  
+                    
+                //     const newChoice = {color : choice.color, quantity: updatedQuantity};
+                    
+                //     // console.log('parseCurrentLs', parseCurrentLs);
+                //     removedEl= parseCurrentLs.splice(indexOfObject2, 1);
+                //     // console.log('parseCurrentLs', parseCurrentLs);
+                //     // console.log( 'removedEl', removedEl);
+
+                //     parseCurrentLs.push(newChoice);
+                //     console.log('parseCurrentLs', parseCurrentLs);
+
+                //     const stringifyNewChoice = JSON.stringify(parseCurrentLs);
+                //     const backInLocalStorage= localStorage.setItem(extractId, stringifyNewChoice)
+                    
+                    
+                //     }else {
+                //         console.log('Different color that is not yet in the array. Push choice');
+                    
+                //         parseCurrentLs.push(choice);
+                //         const stringifyNewChoice = JSON.stringify(parseCurrentLs);
+                //         const backInLocalStorage= localStorage.setItem(extractId, stringifyNewChoice)
+
+                //     }
+
+                    
+                   
+                    
+                    
+                    
+                // }
+
+
+
+
+
+
+
+
+            
+            
+    
+         
+                
+        } else  {
+                //_____________________
+            //--> if there is no key with this ID :
+                console.log("This Kanap has never been selected before. Let's add it to the local storage");
+                firstPushInEmptyArray(choice);
+                makeStringThenSetInLS(firstValue);
+        }   
+    }
+}
+)
--- front/js/product.js
+++ front/js/product.js
@@ -19,7 +19,7 @@ fetch('http://localhost:3000/api/products')
     const title = document.getElementById('title');
     const description = document.getElementById('description');
     const price = document.getElementById('price');
-    const colors = document.getElementById('colors');
+    const colors = document.getElementById('color-select');
     
     //
    itemImg.innerHTML = ` <img src="${found['imageUrl']}" alt="${found['altTxt']}">`;
@@ -34,269 +34,123 @@ fetch('http://localhost:3000/api/products')
     console.log("Une erreur s'est produite.", err);
 });
 
+/*
 
-//________________________________________
 
-//____________________________________________________________
- //au click:
-document.getElementById('addToCart').addEventListener('click', 
-function (){
-        //Couleur et quantité*************************
-            //quantité
-            let quantitySelected = document.getElementById('quantity').value;
-            console.log(typeof quantitySelected); //string
-             const quantity = Number(quantitySelected);
-             console.log(typeof quantity);   // number
+document.getElementById('addToCart').addEventListener('click', function(){
+ //Couleur et quantité*************************
+    //quantity
+    let quantityChoice = document.getElementById('quantity').value;
+        // console.log(typeof quantityChoice); //string
+    const quantity = Number(quantityChoice);
+        // console.log(typeof quantity);   // number
+    
+
+    //color
+    const colorChoice = document.getElementById('color-select'); 
+    const color = colorChoice.options[colorChoice.selectedIndex].value; 
+
+    //choice
+    const choice = {color: color, quantity: quantity};
+    const stringifyChoice = JSON.stringify(choice);
+    console.log(choice, stringifyChoice);
+    let putInLocalStorage =[];
+    console.log('putInLocalStorage', putInLocalStorage);
+    
+
+    // if a color is picked
+    if (color !== ""){
+        console.log("The selection is valid");
+        //if local storage is empty
+        if(localStorage.getItem(extractId)){
+            console.log('There is an article with this key in the local storage');
+            //Get the article in the local storage
+            const getTheArticleInTheLs = localStorage.getItem(extractId);
+            console.log('getTheArticleInTheLs', getTheArticleInTheLs);
            
-
-            //couleur
-            const colorSelected = document.getElementById('colors'); 
-            const color = colorSelected.options[colorSelected.selectedIndex].value; 
+            const parseTheResult = JSON.parse(getTheArticleInTheLs);
+            console.log('parseTheResult',parseTheResult);
             
-            let choice = {'color': color, "quantity": quantity};
+                // Parse the article
+                for (let i=0; i<parseTheResult.length; i++) {
+                console.log(parseTheResult[i].color,' parseTheResult color');
+                console.log(choice.color);
+
+                    // If the color is the same:
+                    // 1 -  add quantities of CHOICE and PARSETHERESULT 
+                    // 2 - update the local storage
+                    if(parseTheResult[i].color === choice.color){
+                        console.log( "The color is the same");
+                        // Add the quantities
+                        console.log(parseTheResult[i].quantity,choice.quantity);
+                        const updatedQuantity = parseTheResult[i].quantity + choice.quantity;
+                        console.log('updatedQuantity',updatedQuantity);
+                        const updatedChoice = {color : choice.color, quantity : updatedQuantity};
+                        console.log('updatedChoice', updatedChoice);
+                        // // Update the local storage
+                        putInLocalStorage.push(updatedChoice);
+                        console.log(putInLocalStorage);
+                        const stringifyUpdatedQuantity = JSON.stringify(putInLocalStorage);
+                        console.log(stringifyUpdatedQuantity);
+                        localStorage.setItem(extractId, stringifyUpdatedQuantity);
+
+                    }else {
+                        //1  - get then parse the content of local storage
+                        // 2 loop over the selection
+                        
+
+                            console.log( "The color is different");
+                            //TODO =>add new line to the local storage
+                            //_____________________________
+                            fetch('http://localhost:3000/api/products')
+                            .then(function(res){
+                                console.log(res);   
+                                return res.json();
+                            })
+                            .then(data => { 
+                            const findColor = data.find( element => element['_id'] === extractId);
+                            console.log('findColor', findColor);
+                            console.log(findColor.colors,'findColor.colors' );pl^kopo
+                            const differentColorId = findColor._id;
+                            console.log('differentColorId', differentColorId, extractId);
+                            const differentColor = findColor.colors;
+                            const found = differentColor.find(element => element === color )
+                            console.log('found', found);
+                            const updateColor = {color : found, quantity: quantity};
+                        
+                            console.log('updateColor', updateColor, typeof updateColor);
+                            console.log('parseTheResult', parseTheResult);
+                            parseTheResult.push(updateColor);
+                            console.log('parseTheResult',parseTheResult);
+                            const stringifyNewResult = JSON.stringify(parseTheResult);
+                            console.log('stringifyNewResult',stringifyNewResult, typeof stringifyNewResult);
+                            localStorage.setItem(extractId,stringifyNewResult);
+                        
+                            // if(extractId === localStorage.key && )
+                            
+
+
+                            } )
+                            .catch(error => {
+                                console.log("Une erreur s'est produite");
+                            });
+
+                            //_____________________________
+                        
+                        
+                        }
             
-            if (color !== "") {
-                if (localStorage.getItem(extractId) == null) {
-                    
-                    const stringifierChoice = JSON.stringify([choice]);
-                console.log(stringifierChoice);
-                    const mettreDansLocalStorage = localStorage.setItem(extractId,stringifierChoice);
-                } else{
-                //1 - RECUPERER LES DONNEES DU LOCAL STORAGE
-                const donneesLocalStorage = localStorage.getItem(extractId);
-                console.log('donneesLocalStorage', typeof donneesLocalStorage, donneesLocalStorage);
-                //2 - PARSER LES DONNEES
-                const parserLesDonnees = JSON.parse(donneesLocalStorage);
-                console.log('parserLesDonnees',typeof parserLesDonnees, parserLesDonnees);
-                //3 - COMPARER LES COULEURS DE choice ET parserLesDonnees
-                // console.log(choice[0].color);
-                for (let i = 0; i< parserLesDonnees.length; i++) { 
-                     if (choice.color === parserLesDonnees[i].color){
-                        console.log('Identiques');
-                        const additionnerLesQuantites = choice.quantity + parserLesDonnees[i].quantity;
-                        console.log('additionnerLesQuantites', additionnerLesQuantites);//number
-                        const newChoice = [{'color': color, 'quantity' : additionnerLesQuantites}]
-
-                        const stringifyNewChoice = JSON.stringify(newChoice);
-                        console.log(stringifyNewChoice);
-
-                        localStorage.setItem(extractId,stringifyNewChoice)
-
-
-                    }
-                    else {
-                        console.log('Differents');
-                        const ajouterAuLocalStorage = parserLesDonnees.push(choice);
-                        const nouveauChoixDeCouleur = JSON.stringify(parserLesDonnees);
-                        console.log(nouveauChoixDeCouleur);
-                        localStorage.setItem(extractId, nouveauChoixDeCouleur);
-
-                    }
-                    break;
+           
                 }
+        }else{  
+            console.log('No article with this key in the local storage');
+            putInLocalStorage.push(choice);
+            console.log(putInLocalStorage);
+            const localStorageValue = JSON.stringify(putInLocalStorage);
+            console.log(localStorage);
+            console.log(localStorageValue, typeof localStorageValue);
+            localStorage.setItem(extractId, localStorageValue);
+        }
 
-                  
-
-
-                
-            }
-        
     }
-
-
-
-
-           
-            
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                      //             //****************************
-// //             const choice = [colorChoice, quantityChoice]
-// // console.log('choice', typeof choice,choice); //object
-// //             localStorage.setItem([extractId], [choice]);     
-// //                    const stringifyChoice = JSON.stringify(choice);
-// // console.log(stringifyChoice, 'stringifyChoice');
-// // let setItemInLocalStorage = localStorage.setItem([extractId],[stringifyChoice]);
-// // console.log(localStorage, 'arrayForLocalStorage');
-
-//     if (color !== "") {
-
-  
-//         for (let i=0; i < localStorage.length; i++){
-// //DEPUIS LE LOCAL STORAGE
-// // 1 - RECUPERER LES DONNES DU LOCAL STORAGE
-// const donneesDuLocalStorage = localStorage.getItem([extractId]);
-//     console.log('donneesDuLocalStorage',typeof donneesDuLocalStorage,donneesDuLocalStorage);
-// // 2 - PARSER LES DONNEES
-// const parserLesDonnees = JSON.parse(donneesDuLocalStorage);
-//     console.log('parserLesDonnes', typeof parserLesDonnees, parserLesDonnees);
-// //NOUVELLES DONNEES
-// // 3 - SAUVEGARGER LES NOUVELLES QUANTITES ET DONNEES DANS UN CONSR
-// const choice = [{}]
-//     console.log('choice', typeof choice,choice); //object
-
-// // 4 - METTRE LA CONDITION
-// console.log(parserLesDonnees[0].color);
-// console.log(choice[0].color);
-// console.log(parserLesDonnees[1].quantity);
-// console.log(choice[1].quantity);
-// // 4.1 - SI LES COULEURS SONT IDENTIQUES ==> ADDITIONNER LES QUANTITES
-// if (parserLesDonnees[0].color === choice[0].color) {
-//     const updateQuantites = parserLesDonnees[1].quantity + choice[1].quantity;
-//     console.log(updateQuantites);
-// } else {
-//     // 4.2 - SINON ==> PASSER A L'ETAPE 5 
-//     // 4.2.1 - STRINGIFIER CHOICE ET L'AJOUTER AU LOCAL STORAGE
-//     const stringifierNouvellesDonnees = JSON.stringify(choice);
-//     console.log('stringifierNouvellesDonnees', typeof stringifierNouvellesDonnees, stringifierNouvellesDonnees);
-
-//   localStorage +=  localStorage.setItem([extractId],([stringifierNouvellesDonnees]))
-//     console.log('localstorage', localStorage);
-//  } 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//             // 1 - RECUPERER LES DONNEES DU LOCAL STORAGE
-// let currentCart = [i].getItem([extractId]);
-//             console.log(currentCart, 'currentCart getItem, typeof:', typeof currentCart);
-
-// const parsedCart= JSON.parse(currentCart);
-//             console.log(parsedCart, 'parsedCart, typeof: ', typeof parsedCart);
-            
-// const currentQuantity =parsedCart[1].quantity;
-//             console.log(currentQuantity, 'currentQuantity, type of', typeof currentQuantity); 
-            
-//             // 2 - RECUPERER LA QUANTITE SELECTIONNEE
-//             const addNewquantity = Number(quantitySelected);
-//             console.log(addNewquantity, 'addNewquantity', typeof addNewquantity);            
-//             // 3 - ADDITIONNER LES DEUX RESULTATS
-           
-//             const newQuantity =addNewquantity + currentQuantity;
-//             console.log(newQuantity, 'newQuantity, typeof', typeof newQuantity);
-           
-            
-            
-//             // 4 - REMETTRE LES RESULTATS DANS LE LOCAL STORAGE 
-            
-//             quantityChoice.quantity = newQuantity;
-//             localStorage.setItem([extractId],[stringifyChoice]);
-        
-      
-
-
-
-                    
-//_______________________________________
-
-
-//________________________ WORKS
-// const currentQuantity = parsedCart[quantityChoice.quantity];
-//const currentQuantity = parsedCart[1].quantity;
-//__________________________
-//________________________ TESTS
-//__________________________
-
-
-                      
-                    
-            // for (let i =0; i < choice.length; i++) {
-            //     choice = i;
-            //     console.log(choice);
-            // }
-           
-            // // for(let i = 0; i< localStorage.length; i++) {
-            //     let MyStringifiedCart = JSON.stringify(choice);
-            //     console.log('MyStringifiedCart', MyStringifiedCart,typeof MyStringifiedCart);
-                
-
-          
-            // //1 get the current cart
-            // const currentCart = localStorage.getItem(extractId);
-            // console.log('currentCart',currentCart, typeof currentCart);
-            
-            // // 2 parse the current cart
-            // let myParsedCart= JSON.parse(currentCart)
-            // console.log('myParsedCart',myParsedCart,typeof myParsedCart);
-            
-            // //3 update quantity
-            
-            
-            
-            
-            // //4 stringify because local storage only works with strings
-            
-            // //5 store in LS           
-            // const newCart =  localStorage.setItem(extractId, MyStringifiedCart );
-            // console.log('newCart', newCart, typeof newCart);
-            
-              
-
-
-
-              
-                
-
-
-                
-               
-                //test
-                // console.log( choice, test2,test3);
-            // } (for loop curly bracket)
-        
-                
-
-})
-// localStorage.clear();
+})*/
