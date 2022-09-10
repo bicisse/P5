4f7387fbc91e0fe5a8de673c38c0269faecd6aa7
bicisse <105852207+bicisse@users.noreply.github.com>
2022-09-10 08:23:04 +0200

    total quantité et prix

--- front/js/cart.js
+++ front/js/cart.js
@@ -10,8 +10,21 @@ fetch('http://localhost:3000/api/products')
 })
 
 .then(function(data){
-  
-  
+  let addUpQuantities=[];
+  let addUpPrices = [];
+  function addUp(pushInArray, array) {
+    array.push(pushInArray);
+    console.log('array', array, typeof array);
+     let sum = 0;
+             for (let g = 0; g < array.length; g++) {
+                 sum += array[g];
+             }
+     console.log('sum', sum, typeof sum);
+     return sum
+   }
+
+
+
   for(let i=0; i< localStorage.length; i++) {
     const getKeys = localStorage.key(i);
     console.log('getKeys',getKeys);
@@ -32,28 +45,47 @@ fetch('http://localhost:3000/api/products')
     
     for( let a = 0; a< parseArray.length; a++){
       console.log(parseArray[a]);
-      const itemColor = parseArray[a].color;
-      const itemQuantity = parseArray[a].quantity;
+      const storedColor = parseArray[a].color;
+      const storedQuantity = parseArray[a].quantity;
       const price = data[i]['price'];
-      console.log('price', price, typeof price);
-      console.log('itemQuantity', itemQuantity, typeof itemQuantity);
-      console.log('itemColor', itemColor);
+      const itemQuantity = document.getElementsByClassName('itemQuantity');
+      const totalQuantity = document.getElementById('totalQuantity');
+      const totalPrice = document.getElementById('totalPrice');
     
+    
+      
+     
+      console.log('price', price, typeof price);
+      console.log('storedQuantity', storedQuantity, typeof storedQuantity);
+      console.log('storedColor', storedColor);
+
+
+      //TOTAL
+      // QUANTITY
+      const multiplyPriceByQuantity = price*storedQuantity;
+      console.log('multiplyPriceByQuantity',multiplyPriceByQuantity, typeof multiplyPriceByQuantity);
+      totalQuantity.textContent = addUp(storedQuantity, addUpQuantities);
+  
+      // //PRICE
+      // 
+      totalPrice.textContent = addUp(multiplyPriceByQuantity, addUpPrices);
+
+      //________________
       // Inner HTML
-      cartItems.innerHTML += ` <article class="cart__item" data-id="{${data[i]['_id']}}" data-color="${itemColor}">
+      cartItems.innerHTML += ` <article class="cart__item" data-id="{${data[i]['_id']}}" data-color="${storedColor}">
           <div class="cart__item__img">
             <img src="${data[i]['imageUrl']}" alt="${data[i]['altTxt']}">
           </div>
           <div class="cart__item__content">
             <div class="cart__item__content__description">
               <h2>${data[i]['name']}</h2>
-              <p>${itemColor}</p>
+              <p>${storedColor}</p>
               <p>${price} €</p>
             </div>
             <div class="cart__item__content__settings">
             <div class="cart__item__content__settings__quantity">
             <p>Qté : </p>
-            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${itemQuantity}">
+            <input type="number" class="storedQuantity" name="storedQuantity" min="1" max="100" value="${storedQuantity}">
           </div>
               <div class="cart__item__content__settings__delete">
                 <p class="deleteItem"  >Supprimer</p>
@@ -62,30 +94,17 @@ fetch('http://localhost:3000/api/products')
           </div>
         </article> `
         //End Inner HTML
-      
-      //
-      const totalItemQuantity = document.getElementById('totalQuantity');
-      const totalPrice = document.getElementById('totalPrice');
-      let test = [];
-      
-      let addition = (itemQuantity)*(price);
-      console.log('addition', addition, typeof addition);
-      
-      //Total Quantity
 
         // delete Item
         
-    } //loop A
+         
+          // loop J
 
-    for(let j=0; j< deleteItem.length; j++){
-      const button = deleteItem[j];
-        button.addEventListener('click', function(event){
-          window.alert("L'article a bien été supprimé du panier")
-        const buttonClicked = event.target;
-        buttonClicked.parentElement.parentElement.parentElement.parentElement.remove();
-        })
-      console.log('removeFromLs', removeFromLs);
-      }// loop J
+        //__________________________
+        
+      } //loop A
+
+   
   }//loop I
 //
 })
