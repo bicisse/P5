9cc38a6ff7621dd338bd9eed3bc88610beacee59
bicisse <105852207+bicisse@users.noreply.github.com>
2022-09-13 10:54:31 +0200

    Form validation

--- front/js/form.js
+++ front/js/form.js
@@ -10,7 +10,8 @@ const cityErrorMsg = document.getElementById('cityErrorMsg');
 const email  = document.getElementById('email');
 const emailErrorMsg = document.getElementById('emailErrorMsg');
 const order = document.getElementById('order');
-const formInput = document.querySelectorAll('');
+const formInput = document.getElementsByClassName('cart__order__form__question');
+// const formInput = document.querySelectorAll('input [type="text"]');
 
 
 console.log(orderForm);
@@ -24,81 +25,77 @@ let letters;
 //     return letters;
 //   }
 
+let trying = 'Harry';
 
+// function allLetter(inputtxt)
+//   {
+//    var letters = /^[A-Za-z]+$/;
+//    if(inputtxt.value.test(letters))
+//      {
+//       return true;
+//      }
+//    else
+//      {
+//      alert("message");
+//      return false;
+//      }
+//   }
 
-order.addEventListener( 'click', function (e){
-    const errorMsgArray = [];
-    let errorMsg;
-    console.log(formInput);
+console.log(trying);  
+// allLetter(test);
 
-    for (let i=0; i<formInput.length;i++){
-        const question = formInput[i];
-        
-        if (question ==" ") {
+function onlyLetters(str) {
+    return /^[0-9]+$/.test(str);
+  }
+  
+  console.log(onlyLetters('hello')); // 👉️ true
+  console.log(onlyLetters('Johnson')); // 👉️ false
+  console.log(onlyLetters('Bintou-Fatima')); // 👉️ false
 
-            console.log('working?');
-            e.defaultPrevented();
-        }
-    
+  
+console.log(order);
+order.addEventListener( 'click', function(event){
 
-    }
+    let errorMsgArray = [];
         if (firstName.value == " ") {
-            errorMsgArray.push('error');
-            errorMsg = "firstName";
+            errorMsgArray.push('firstName');
+            firstNameErrorMsg.textContent = "Veuillez renseigner votre prénom";
+
         }
         if (lastName.value === " ") {
-            errorMsgArray.push('error');
-            errorMsg = "lastName";
+            errorMsgArray.push('lastName');
+            lastNameErrorMsg.textContent  = "Veuillez renseigner votre nom de famille";
         }
 
         if (address.value === " ") {
-            errorMsgArray.push('error');
-            errorMsg = "address";
+            errorMsgArray.push('address');
+            addressErrorMsg.textContent  = "Veuillez renseigner votre adresse";
         }
         if (city.value === " ") {
-            errorMsgArray.push('error');
-            errorMsg = "city";
+            errorMsgArray.push('city');
+            cityErrorMsg.textContent  = "Veuillez renseigner votre ville de résidence";
         }
         if (email.value === " ") {
-            errorMsgArray.push('error');
-            errorMsg = "email";
-        }
-        /*
-            // lastNameErrorMsg.textContent = "Veuillez indiquer votre nom.";
-            // addressErrorMsg.textContent = "Veuillez indiquer votre adresse.";
-            // cityErrorMsg.textContent = "Veuillez indiquer votre ville.";*/
-            
+            errorMsgArray.push('email');
+            emailErrorMsg.textContent  = "Veuillez renseigner votre email";}
+       
     console.log('errorMsgArray', errorMsgArray);
-        
-    switch(errorMsg){
-        case "firstName" :
-            console.log('firstNAme indeed');
-        break;
-        case "lastName" :
-            console.log('firstNAme indeed');
-        break;
-        case "city" :
-            console.log('firstNAme indeed');
-        break;
-        case "address" :
-            console.log('firstNAme indeed');
-        break;
-        case "email" :
-            console.log('firstNAme indeed');
-        break;
-        default : console.log('Not working');
+        console.log(errorMsgArray[0]);
+    //CHECKING THERE ARE ONLY LETTERS
+    if (onlyLetters(firstName.value) || onlyLetters(lastName.value)){
+        alert("Merci de n'utiliser que des lettres");
+        errorMsgArray.push('only letters')
     }
-
-
-        console.log(2);
-    
+        
         if (errorMsgArray.length > 0) {
     
-            e.preventDefault();
+            event.preventDefault();
             console.log(errorMsgArray);
         }
-    
+    console.log(errorMsgArray);
+        while(errorMsgArray.length >0){
+        errorMsgArray.pop();
     }
-    
-
-);
+    console.log(errorMsgArray);
+        
+    });
